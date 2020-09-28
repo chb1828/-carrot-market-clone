cd9f4f879f9ad2d8b51405a91bd7f9a81359772c
bomi <chb1828@naver.com>
2020-09-28 14:10:11 +0900

    동적 입력창 생성

--- lib/nearby/nearby.dart
+++ lib/nearby/nearby.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:market_clone/nearby/changeString.dart';
 
@@ -8,30 +7,41 @@ class NearBy extends StatefulWidget {
 }
 
 class _NearByState extends State<NearBy> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: SingleChildScrollView(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        children: [
-          ChangeString(),
-          SizedBox(height: 20),
-          _imageSection1,
-          _searchSection,
-        ],
-      ),
-    ));
+        backgroundColor: Colors.white,
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  SizedBox(height: 30),
+                  ChangeString(),
+                  SizedBox(height: 20),
+                  _imageSection1
+                ],
+              ),
+            ),
+            SliverAppBar(
+              centerTitle: true,
+              pinned: true,
+              backgroundColor: Colors.white,
+              title: _searchSection,
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  Text("하이")
+                ],
+              ),
+            ),
+          ],
+        )
+    );
   }
-  
+
   Widget _imageSection1 = Container(
     padding: EdgeInsets.all(10),
     child: Column(
@@ -39,32 +49,38 @@ class _NearByState extends State<NearBy> {
         Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Image.asset('assets/images/house.png',width: 300,height: 130)
+            Image.asset('assets/images/house.png', width: 300, height: 130)
           ],
         ),
-        SizedBox(height: 30,)
       ],
     ),
   );
-  
+
   Widget _searchSection = Container(
-    child: Column(
-      children: [
-        TextField(
-          decoration: InputDecoration(
-            hintStyle: TextStyle(fontSize: 17,height: 0.5),
+      height: 40.0,
+      child: TextField(
+        style: TextStyle(
+          fontSize: 20.0,
+          height: 1.0,
+          color: Colors.white
+        ),
+        decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            hintStyle: TextStyle(fontSize: 17, height: 0.5),
             hintText: "내 근처에서 주변 가게를 찾아보세요",
             prefixIcon: Icon(Icons.search),
+            contentPadding: const EdgeInsets.all(20.0),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black45,style: BorderStyle.solid,width: 1)
+            ),
             border: OutlineInputBorder(
-              borderRadius: const BorderRadius.all(const Radius.circular(1.0))
+                borderRadius: const BorderRadius.all(const Radius.circular(5.0))
             )
-          ),
-        )
-      ],
-    ),
+        ),
+      )
   );
 
 }
-
 
 
